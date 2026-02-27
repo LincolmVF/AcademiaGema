91ffd835c34e022cb30779feeea1bdeadb7d415a
YazminSuybate <yaaazscx@gmail.com>
2026-02-26 22:57:33 -0500

    Actualizar configuración de Vite para permitir todos los hosts en el entorno de vista previa

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: [
-      'all'
-    ]
+    allowedHosts: true, 
+    port: process.env.PORT || 8080,
+    host: '0.0.0.0'
   },
   server: {
-    allowedHosts: true
+    allowedHosts: true 
   }
 })
